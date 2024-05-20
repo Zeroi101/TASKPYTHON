abeed5e99f7191b032ac470596678198b0c262a0
Zeroi101 <167221558+Zeroi101@users.noreply.github.com>
2024-05-20 19:39:08 +0300

    Add files via upload

---
+++ DZ6/unit_test/unit_test.py
@@ -0,0 +1,15 @@
+﻿#coding=utf-8
+import unittest
+from DZ63 import factorial
+
+class Testfactorial(unittest.TestCase):
+    def setUp(self) -> None:
+        print("setUp")
+    def test_factorial(self):
+        self.assertEqual(factorial(2), 2)
+        self.assertEqual(factorial(0), 1)
+        self.assertRaises(ValueError, factorial, -1, 2)
+        self.assertRaises(ValueError, factorial, 1024, 2)
+        self.assertEqual(factorial(3), 6)
+    def tearDown(self) -> None:
+        print("tearDown")
